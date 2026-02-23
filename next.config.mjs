4bb19c8d8e507aa681d926023e455bd777e7d82e
AmanVatsSharma <aman95026@gmail.com>
2026-02-23 19:39:52 +0530

    Add .env.example file, update package dependencies, and remove unused Clerk packages

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const nextConfig = {
         remotePatterns: [
             { protocol: 'https', hostname: 'uploadthing.com' },
             { protocol: 'https', hostname: 'utfs.io' },
-            { protocol: 'https', hostname: 'img.clerk.com' },
             { protocol: 'https', hostname: 'subdomain' },
             { protocol: 'https', hostname: 'files.stripe.com' },
         ],
