09181fd956a702f571ded0d1d32bd0158e8d839a
AmanVatsSharma <aman95026@gmail.com>
2026-02-23 20:21:10 +0530

    Refactor layout components to integrate Clerk for authentication, update font to DM Sans, and add modal and toaster providers. Introduce new loading component for agency pages.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,44 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { AuthSessionProvider } from "@/components/providers/session-provider";
-import { ThemeProvider } from "@/components/theme-provider";
+import type { Metadata } from 'next'
+import { DM_Sans } from 'next/font/google'
+import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
+import { ThemeProvider } from '@/providers/theme-provider'
+import ModalProvider from '@/providers/modal-provider'
+import { Toaster } from '@/components/ui/toaster'
+import { Toaster as SonnarToaster } from '@/components/ui/sonner'
 
-const inter = Inter({ subsets: ["latin"] });
+const font = DM_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Web-Builder-Test",
-  description: "All in one Agency Solution",
-};
+  title: 'Plura',
+  description: 'All in one Agency Solution',
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en">
-      <body className="">
-        <AuthSessionProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
+    <html
+      lang="en"
+      suppressHydrationWarning
+    >
+      <body className={font.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ModalProvider>
             {children}
-          </ThemeProvider>
-        </AuthSessionProvider>
+            <Toaster />
+            <SonnarToaster position="bottom-left" />
+          </ModalProvider>
+        </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
