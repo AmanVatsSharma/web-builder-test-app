553a57e05d78e9236967460846d3e841d841b88e
AmanVatsSharma <aman95026@gmail.com>
2026-02-24 11:49:44 +0530

    Remove unused favicon and clean up layout components by integrating AuthSessionProvider. Simplify main layout by removing ClerkProvider and associated imports. Delete obsolete domain and path pages to streamline the application structure.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
 import type { Metadata } from 'next'
 import { DM_Sans } from 'next/font/google'
 import './globals.css'
-import { ClerkProvider } from '@clerk/nextjs'
-import { dark } from '@clerk/themes'
 import { ThemeProvider } from '@/providers/theme-provider'
 import ModalProvider from '@/providers/modal-provider'
 import { Toaster } from '@/components/ui/toaster'
 import { Toaster as SonnarToaster } from '@/components/ui/sonner'
+import { AuthSessionProvider } from '@/components/providers/session-provider'
 
 const font = DM_Sans({ subsets: ['latin'] })
 
@@ -26,18 +25,20 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <body className={font.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <ModalProvider>
-            {children}
-            <Toaster />
-            <SonnarToaster position="bottom-left" />
-          </ModalProvider>
-        </ThemeProvider>
+        <AuthSessionProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <ModalProvider>
+              {children}
+              <Toaster />
+              <SonnarToaster position="bottom-left" />
+            </ModalProvider>
+          </ThemeProvider>
+        </AuthSessionProvider>
       </body>
     </html>
   )
