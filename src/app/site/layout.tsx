a9688107ebe94e036002d6572ab876bc5ea2bc4a
AmanVatsSharma <aman95026@gmail.com>
2026-02-24 19:19:48 +0530

    Add upload route and update layout and page components
    - Introduced a new route for file uploads using the Uploadthing library, enabling GET and POST request handling.
    - Updated layout component to ensure consistent class naming conventions.
    - Enhanced the home page by integrating Stripe pricing data, improving the display of subscription options and features.
    - Refactored card rendering logic to utilize Stripe's pricing API, ensuring accurate pricing information is presented to users.

--- src/app/site/layout.tsx
+++ src/app/site/layout.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className='h-full'>
+    <main className="h-full">
       <Navigation />
       {children}
     </main>
